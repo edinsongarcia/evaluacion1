52090df3a68f8571d84cae51581a3ce25bb5b999
edinsongarcia <edinsong61@gmail.com>
2016-06-22 14:34:35 -0500

    Trabajo JQuery

---
+++ funciones.js
@@ -0,0 +1,27 @@
+var x;
+x=$(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos()
+{
+  var x;
+  x=$("#publicidad");
+  x.click(presionPublicidad)
+  x=$("#encabezado");
+  x.click(presionencabezado)
+}
+
+function presionPublicidad()
+{
+  var x;
+  x=$(this);
+  x.css("background","#fff");
+}
+function presionencabezado()
+{
+  var x;
+  x=$(this);
+  x.css("color","blue");
+  x.css("font-family","arial");
+}
+
